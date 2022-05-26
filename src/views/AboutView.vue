86603b2ac0cb40a2302db70f5f77db1fc8122445
suwenlong <958089904@qq.com>
2022-05-26 14:54:50 +0800

    点线面

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,118 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <div class="container">
+      <div id="cesiumContainer"></div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  // 项目基于二次封装cesium的超图Api
+  // 面的点击效果
+  data() {
+    return {
+      _viewer: null, //数据前加_不会被监听
+      _handler: null,
+      // _preLineId:null,
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      // 渲染出地球球体
+      this._viewer = new Cesium.Viewer("cesiumContainer", {});
+      var promise = Cesium.GeoJsonDataSource.load(
+        "https://geo.datav.aliyun.com/areas_v2/bound/110000_full.json"
+      );
+      promise.then((datasource) => {
+        this._viewer.dataSources.add(datasource);
+        var entities = datasource.entities.values;
+        for (var i = 0; i < entities.length; i++) {
+          var entity = entities[i];
+          entity.polygon.material = Cesium.Color.SKYBLUE;
+          entity.polygon.extrudedHeight = 1000;
+        }
+      });
+      // 鼠标点击面获得动效线效果
+      this._handler = new Cesium.ScreenSpaceEventHandler(this._viewer.scene.canvas);
+      this._handler.setInputAction((e) => {
+        var pick = this._viewer.scene.pick(e.position);
+        if (Cesium.defined(pick) && pick.id) {
+          var feature = pick.id;
+          this._viewer.entities.removeById("select_grid");
+          this._viewer.entities.removeById(`line_${feature.id}`);
+          let positions = feature.polygon.hierarchy.getValue(
+            Cesium.JulianDate.now()
+          ).positions;
+          this._viewer.entities.add({
+            id: "select_grid",
+            polygon: {
+              hierarchy: positions,
+              material: this.getColorRamp(
+                "rgba(0, 255, 255,1)",
+                "rgba(255,0,0,0.3)"
+              ),
+              height: 499,
+            },
+          });
+        }
+      }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+      this._handler.setInputAction((movement) => {
+        var pickFeature =
+          this._viewer.scene.pick(movement.endPosition) &&
+          this._viewer.scene.pick(movement.endPosition).id;
+        if (Cesium.defined(pickFeature) && this.preLineId !== pickFeature.id) {
+          this.preLineId &&
+            this._viewer.entities.removeById(`line_${this.preLineId}`);
+          this.preLineId = pickFeature.id;
+          this._viewer.entities.add({
+            id: "line_" + pickFeature.id,
+            name: "line_" + pickFeature.name,
+            polyline: {
+              positions: pickFeature.polygon.hierarchy.getValue(
+                Cesium.JulianDate.now()
+              ).positions,
+              width: 8,
+              material: new Cesium.PolylineTrailMaterialProperty({
+                // 尾迹线材质
+                color: Cesium.Color.AQUA,
+                trailLength: 0.9,
+                period: 1,
+              }),
+            },
+          });
+        } else {
+          this.preLineId &&
+            this._viewer.entities.removeById(`line_${this.preLineId}`);
+          this.preLineId = null;
+        }
+      }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+    },
+    // canvas绘制颜色渐变
+    getColorRamp(rampColor, centerColor) {
+      var ramp = document.createElement("canvas");
+      ramp.width = 50;
+      ramp.height = 50;
+      var ctx = ramp.getContext("2d");
+      //通过createRadialGradient创建一个颜色渐变的对象
+      var grd = ctx.createRadialGradient(25, 25, 0, 25, 25, 50);
+      grd.addColorStop(0, centerColor); // "rgba(255,255,255,0)"
+      grd.addColorStop(1, rampColor);
+
+      ctx.fillStyle = grd;
+      ctx.fillRect(0, 0, 50, 50);
+
+      // return ramp;
+
+      return new Cesium.ImageMaterialProperty({
+        image: ramp,
+        transparent: true,
+      });
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,6 +12,7 @@ export default {
   data() {
     return {
       _viewer: null, //数据前加_不会被监听
+      _handler: null,
     };
   },
   mounted() {
@@ -23,8 +24,14 @@ export default {
       this._viewer = new Cesium.Viewer("cesiumContainer", {});
       // 添加底图
       var layer = this._viewer.imageryLayers.addImageryProvider(
-        new Cesium.ArcGisMapServerImageryProvider({
-          url: "http://cache1.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineCommunity/MapServer",
+        // 浅色底图 提供由ArcGIS MapServer托管的平铺图像。
+        // new Cesium.ArcGisMapServerImageryProvider({
+        //   url: "http://cache1.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineCommunity/MapServer",
+        // })
+        // 深色底图 通过使用指定的URL模板请求图块来提供图像。
+        new Cesium.UrlTemplateImageryProvider({
+          // url: "http://cache1.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineCommunity/MapServer",
+          url: "https://map.geoq.cn/arcgis/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}",
         })
       );
       //   初始化场景位置 可以通过setView和flyTo飞向目的地  还有rotate:旋转 zoom:缩放 pan:平移  后面这三种方式配置的参数有所不同
@@ -32,15 +39,98 @@ export default {
         // 初始化相机经纬度（这里使用了经纬度转换世界坐标的方法）
         // destination目的地   可以是Cartesian3或者Rectangle   笛卡尔或者直角坐标系
         destination: new Cesium.Cartesian3.fromDegrees(
-          103.98706845922472,
-          30.604509912288446,
-          1000,//弧度，差不多等于放大倍数
+          121.54035,
+          38.92146,
+          2000 //弧度，差不多等于放大倍数
         ),
         // orientation可以是heading | pitch | roll | direction | up
         orientation: {
-          heading: Cesium.Math.toRadians(0.0),//坐标偏移量
+          heading: Cesium.Math.toRadians(0.0), //坐标偏移量
           pitch: Cesium.Math.toRadians(-25.0), //从上往下看为-90  倾斜度
           roll: 0,
+        },
+      });
+      // 先注册全局handler，再通过setInputAction注册鼠标左击事件，获得相应的面，获取到对应的属性或者自定义操作
+      this._handler = new Cesium.ScreenSpaceEventHandler(
+        this._viewer.scene.canvas
+      );
+      this._handler.setInputAction((e) => {
+        var pick = this._viewer.scene.pick(e.position);
+        // 这里点击的id为我们拾取到的实体
+        console.log(e, pick);
+      }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+      this.addBillboard();
+      this.addPolyline();
+      this.addPolygon();
+    },
+    // 点
+    addBillboard() {
+      this._viewer.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(121.54035, 38.92146, 2000),
+        billboard: {
+          // image: "https://img2.baidu.com/it/u=2243064028,1144835942&fm=253&fmt=auto&app=138&f=JPEG?w=667&h=500", // default: undefined
+          image: "./monitoring.png", // default: undefined
+
+          show: true, // default
+          pixelOffset: new Cesium.Cartesian2(0, -50), // default: (0, 0)
+          eyeOffset: new Cesium.Cartesian3(0.0, 0.0, 0.0), // default
+          horizontalOrigin: Cesium.HorizontalOrigin.CENTER, // default
+          verticalOrigin: Cesium.VerticalOrigin.BOTTOM, // default: CENTER
+          scale: 2.0, // default: 1.0
+          color: Cesium.Color.WHITE, // default: WHITE
+          rotation: Cesium.Math.PI_OVER_FOUR, // default: 0.0
+          alignedAxis: Cesium.Cartesian3.ZERO, // default
+          width: 25, // default: undefined
+          height: 25, // default: undefined
+        },
+      });
+    },
+    // 线
+    addPolyline() {
+      this._viewer.entities.add({
+        polyline: {
+          // 用笛卡尔做表集绘制图形
+          positions: Cesium.Cartesian3.fromDegreesArray([
+            121.534575, 38.926131, 121.537579, 38.92543, 121.541784, 38.924578,
+            121.543973, 38.924144, 121.545947, 38.923944,
+          ]),
+          width: 4,
+          material: Cesium.Color.DARKORANGE.withAlpha(0.3), //线的材质
+          // clampToGround: true,//是否贴地渲染
+          // show: true,
+        },
+      });
+      this._viewer.entities.add({
+        // name:entity.name,
+        polyline: {
+          positions: Cesium.Cartesian3.fromDegreesArray([
+            121.534575, 38.926131, 121.537579, 38.92543, 121.541784, 38.924578,
+            121.543973, 38.924144, 121.545947, 38.923944,
+          ]),
+          width: 4, // 线的宽度，像素为单位
+          material: new Cesium.PolylineTrailMaterialProperty({
+            // 尾迹线材质
+            color: Cesium.Color.GOLD,
+            trailLength: 0.4,
+            period: 3.0,
+          }),
+          // clampToGround: true,
+          // show: true,
+        },
+      });
+    },
+    // 面
+    addPolygon() {
+      this._viewer.entities.add({
+        polygon: {
+          hierarchy: Cesium.Cartesian3.fromDegreesArray([
+            121.539208, 38.924962, 121.539176, 38.924737, 121.540195, 38.924486,
+            121.540281, 38.924737,
+          ]),
+          extrudedHeight: 50,
+          material: Cesium.Color.WHITESMOKE,
+          // closeTop: false,
+          // closeBottom: false,
         },
       });
     },
